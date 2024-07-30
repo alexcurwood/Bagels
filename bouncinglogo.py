0d1e803597617bcf591e3f6320f33da23ce35aee
alexcurwood <149108865+alexcurwood@users.noreply.github.com>
2024-07-30 15:40:49 +0100

    complete bouncinglogo and update README

--- bouncinglogo.py
+++ bouncinglogo.py
@@ -48,19 +48,84 @@ def main():
             originalDirection = logo[DIR]
 
             if logo[X] == 0 and logo[Y] ==0:
-                logo[DIR] == DOWN_RIGHT
+                logo[DIR] = DOWN_RIGHT
                 cornerBounces += 1
             
             elif logo[X] == 0 and logo[Y] == HEIGHT -1:
-                logo[DIR] == UP_RIGHT
+                logo[DIR] = UP_RIGHT
                 cornerBounces += 1
 
             
             elif logo[X] == WIDTH - 3 and logo[Y] == 0:
-                logo[DIR] == DOWN_LEFT
+                logo[DIR] = DOWN_LEFT
                 cornerBounces += 1
 
             
             elif logo[X] == 0 and logo[Y] == HEIGHT -1:
-                logo[DIR] == UP_RIGHT
+                logo[DIR] = UP_RIGHT
                 cornerBounces += 1
+
+            elif logo[X] == 0 and logo[DIR] == UP_LEFT:
+                logo[DIR] = UP_RIGHT
+
+            elif logo[X] == 0 and logo[DIR] == DOWN_LEFT:
+                logo[DIR] = DOWN_RIGHT
+
+            elif logo[X] == WIDTH - 3 and logo[DIR] == UP_RIGHT:
+                logo[DIR] = UP_LEFT
+            
+            elif logo[X] == WIDTH - 3 and logo[DIR] == DOWN_RIGHT:
+                logo[DIR] = DOWN_LEFT
+            
+            elif logo[Y] == 0 and logo[DIR] == UP_LEFT:
+                logo[DIR] = DOWN_LEFT
+            
+            elif logo[Y] == 0 and logo[DIR] == UP_RIGHT:
+                logo[DIR] = DOWN_RIGHT
+
+            elif logo[Y] == HEIGHT - 1 and logo[DIR] == DOWN_LEFT:
+                logo[DIR] = UP_LEFT
+
+            elif logo[Y] == HEIGHT - 1 and logo[DIR] == DOWN_RIGHT:
+                logo[DIR] = UP_RIGHT
+
+            if logo[DIR] != originalDirection:
+                logo[COLOUR] = random.choice(COLOURS)
+
+            if logo[DIR] == UP_RIGHT:
+                logo[X] += 2
+                logo[Y] -= 1
+
+            if logo[DIR] == UP_LEFT:
+                logo[X] -= 2
+                logo[Y] -= 1
+
+            if logo[DIR] == DOWN_RIGHT:
+                logo[X] += 2
+                logo[Y] += 1
+
+            if logo[DIR] == DOWN_LEFT:
+                logo[X] -= 2
+                logo[Y] += 1
+
+        bext.goto(5,0)
+        bext.fg('white')
+        print('Corner bounces: ', cornerBounces, end='')
+
+        for logo in logos:
+            bext.goto(logo[X], logo[Y])
+            bext.fg(logo[COLOUR])
+            print('DVD', end='')
+
+        bext.goto(0,0)
+
+        sys.stdout.flush()
+        time.sleep(PAUSE_AMOUNT)
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print()
+        print('Bouncing Logo')
+        sys.exit()
